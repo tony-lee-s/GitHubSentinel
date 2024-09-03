974d4dfaff3115015dabcc4e7c192477ede4a15f
Tony_Lee <lds8988@qq.com>
2024-09-03 18:41:23 +0800

    添加订阅 hacker news 的功能

--- src/llm.py
+++ src/llm.py
@@ -3,6 +3,7 @@ import json
 from openai import AzureOpenAI  # 导入OpenAI库用于访问GPT模型
 from logger import LOG  # 导入日志模块
 
+
 class LLM:
     def __init__(self):
         # 创建一个OpenAI客户端实例
@@ -11,14 +12,16 @@ class LLM:
             api_version="2024-02-15-preview",
             azure_endpoint=os.getenv("OPENAI_API_BASE")
         )
-        # 从TXT文件加载提示信息
-        with open("prompts/report_prompt.txt", "r", encoding='utf-8') as file:
-            self.system_prompt = file.read()
 
-    def generate_daily_report(self, markdown_content, dry_run=False):
+    def generate_report(self, prompt_file_name, markdown_content, dry_run=False):
+
+        # 从TXT文件加载提示信息
+        with open(f"prompts/{prompt_file_name}", "r", encoding='utf-8') as file:
+            system_prompt = file.read()
+
         # 使用从TXT文件加载的提示信息
         messages = [
-            {"role": "system", "content": self.system_prompt},
+            {"role": "system", "content": system_prompt},
             {"role": "user", "content": markdown_content},
         ]
 
@@ -34,7 +37,7 @@ class LLM:
 
         # 日志记录开始生成报告
         LOG.info("使用 GPT 模型开始生成报告。")
-        
+
         try:
             # 调用OpenAI GPT模型生成报告
             response = self.client.chat.completions.create(
@@ -47,4 +50,10 @@ class LLM:
         except Exception as e:
             # 如果在请求过程中出现异常，记录错误并抛出
             LOG.error(f"生成报告时发生错误：{e}")
-            raise
+
+    def generate_daily_report(self, markdown_content, dry_run=False):
+        return self.generate_report("report_prompt.txt", markdown_content, dry_run)
+
+    def generate_hacker_news_report(self, markdown_content, dry_run=False):
+        return self.generate_report("hacker_news_prompt.txt", markdown_content, dry_run)
+
